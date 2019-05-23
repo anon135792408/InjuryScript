f9d8f667a2cf275515acbff90bae2e7fabc62db3
Zulfurix <zulfurix@gmail.com>
2019-05-24 00:59:16 +0100

    First basic system tests

---
+++ InjuryScript/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace InjuryScript
+{
+    class Main : BaseScript
+    {
+        public Main()
+        {
+            Tick += ApplyEffectTick;
+        }
+
+        private async Task ApplyEffectTick()
+        {
+            await Delay(250);
+
+            if (Game.PlayerPed.Health < 100)
+            {
+                if (Status.CanWalkingEffectsBeApplied())
+                {
+                    Debug.WriteLine("Applying Effects!");
+                }
+            }
+        }
+    }
+}
---
+++ InjuryScript/Status.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace InjuryScript
+{
+    public class Status : BaseScript
+    {
+        private Status()
+        {
+        }
+
+        public static bool CanWalkingEffectsBeApplied()
+        {
+            if (Game.PlayerPed.IsInVehicle())
+            {
+                return false;
+            }
+            else if (Game.PlayerPed.IsFalling)
+            {
+                return false;
+            }
+            else if (Game.PlayerPed.IsDead)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
