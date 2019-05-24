6366539366172bab8c7e8a6ec49c54b334421c57
Zulfurix <zulfurix@gmail.com>
2019-05-24 01:08:57 +0100

    Diversity for types of injury clipsets

--- InjuryScript/Main.cs
+++ InjuryScript/Main.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
 
 namespace InjuryScript
 {
@@ -18,12 +19,33 @@ namespace InjuryScript
         {
             await Delay(250);
 
-            if (Game.PlayerPed.Health < 100)
+            float health = Game.PlayerPed.Health;
+
+            if (health <= 60 && health > 30)
             {
                 if (Status.CanWalkingEffectsBeApplied())
                 {
-                    Debug.WriteLine("Applying Effects!");
+                    if (!HasAnimSetLoaded("move_m@injured"))
+                        RequestAnimSet("move_m@injured");
+
+                    SetPedMovementClipset(Game.PlayerPed.Handle, "move_m@injured", 1f);
                 }
+            }
+            else if (health < 31)
+            {
+                if (Status.CanWalkingEffectsBeApplied())
+                {
+                    if (!HasAnimSetLoaded("move_m@drunk@verydrunk"))
+                        RequestAnimSet("move_m@drunk@verydrunk");
+
+                    SetPedMovementClipset(Game.PlayerPed.Handle, "move_m@drunk@verydrunk", 1f);
+                }
+            }
+            else
+            {
+                ResetPedMovementClipset(Game.PlayerPed.Handle, 1f);
+                ResetPedWeaponMovementClipset(Game.PlayerPed.Handle);
+                ResetPedStrafeClipset(Game.PlayerPed.Handle);
             }
         }
     }
